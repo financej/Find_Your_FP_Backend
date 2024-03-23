a8ccc04108db802cd81011cc3e6ca12a4436f763
brotherstone97 <brotherstone97@gmail.com>
2024-03-23 20:19:57 +0900

    feat: azure openAI client 반환 메서드 구현

---
+++ backend/src/main/java/com/metlife/team09/domain/openai/OpenAiConfig.java
@@ -0,0 +1,27 @@
+package com.metlife.team09.domain.openai;
+
+import com.azure.ai.openai.OpenAIClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.core.credential.AzureKeyCredential;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OpenAiConfig {
+    @Value("${GPT4.end-point}")
+    private String endPoint;
+
+    @Value("${GPT4.key1}")
+    private String key1;
+
+    @Value("${GPT4.model}")
+    private String modelId;
+    public OpenAIClient getOpenAiClient(){
+        return new OpenAIClientBuilder()
+                .endpoint(endPoint)
+                .credential(new AzureKeyCredential(key1))
+                .buildClient();
+    }
+}
