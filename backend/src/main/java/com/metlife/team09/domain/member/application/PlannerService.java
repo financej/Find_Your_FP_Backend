602c1a1c2d4106562fcdd3da6bb8f86d1a9bcba7
jongmyung <lanto12@naver.com>
2024-03-23 21:29:40 +0900

    feat: Planner List 조회 API 구현

---
+++ backend/src/main/java/com/metlife/team09/domain/member/application/PlannerService.java
@@ -0,0 +1,32 @@
+package com.metlife.team09.domain.member.application;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.metlife.team09.domain.member.application.dto.PlannerListRequestDto;
+import com.metlife.team09.domain.member.application.dto.PlannerListResponseDto;
+import com.metlife.team09.domain.member.persistence.Member;
+import com.metlife.team09.domain.member.persistence.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PlannerService {
+
+	private final MemberRepository memberRepository;
+
+	public List<PlannerListResponseDto> getPlanners(PlannerListRequestDto plannerListRequestDto) {
+		List<Member> allByAddress_dong = memberRepository.findAllByAddress_DongAndIsAdminTrue(plannerListRequestDto.dong());
+		return allByAddress_dong.stream()
+			.map(member -> new PlannerListResponseDto(
+				member.getId(),
+				member.getEmail(),
+				member.getAddress() != null ? member.getAddress().getSiDo()+" "+member.getAddress().getSiGunGu()+" "+member.getAddress().getDong() : null
+			))
+			.collect(Collectors.toList());
+	}
+
+}
