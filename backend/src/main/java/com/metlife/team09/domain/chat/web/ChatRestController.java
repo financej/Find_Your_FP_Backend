c515f243f4ca73764f57986e92ea1c79d9dcae21
leeseunghee00 <lsh9246@gmail.com>
2024-03-24 02:20:35 +0900

    fix: import ChatLogSerivce, ChatLog 추가

--- backend/src/main/java/com/metlife/team09/domain/chat/web/ChatRestController.java
+++ backend/src/main/java/com/metlife/team09/domain/chat/web/ChatRestController.java
@@ -1,12 +1,9 @@
 package com.metlife.team09.domain.chat.web;
 
+import com.metlife.team09.domain.chat.application.ChatLogService;
+import com.metlife.team09.domain.chat.persistence.ChatLog;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.metlife.team09.domain.chat.application.ChatService;
 import com.metlife.team09.domain.chat.application.LogsConvertUtil;
@@ -17,6 +14,8 @@ import com.metlife.team09.domain.chat.application.dto.EndChatRoomRequestDto;
 import com.metlife.team09.domain.chat.persistence.Chat;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 
 @RestController
